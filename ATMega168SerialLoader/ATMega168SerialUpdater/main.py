c32a39ac4763d8530b0297690fa37fd985467593
lucami <lucaminotti1988@gmail.com>
2024-05-08 22:48:06 +0200

    integrated MCU communication

--- ATMega168SerialLoader/ATMega168SerialUpdater/main.py
+++ ATMega168SerialLoader/ATMega168SerialUpdater/main.py
@@ -1,9 +1,32 @@
 from serial import Serial
 
-if __name__ == '__main__':
-    s = Serial("/dev/cu.SLAB_USBtoUART")
-    s.baudrate=9600
-    while 1:
-        print(s.read())
 
-    print("Ciao!")
+def openfile():
+    return open("blink.bin", mode="rb")
+
+
+def serial_open():
+    s = Serial("/dev/cu.SLAB_USBtoUART")
+    s.baudrate = 9600
+    return s
+
+
+def serial_comm(s):
+    while 1:
+        sentence = s.readline()
+        if b"Boot" in sentence:
+            print("Received bootloader string")
+            return 1
+
+
+if __name__ == '__main__':
+    s = serial_open()
+    serial_comm(s)
+    s.write(b"a")
+    print(s.read())
+    hex_file = []
+    f = openfile()
+    readed = f.read()
+
+    for i in readed:
+        print(hex(i), end=" ")
